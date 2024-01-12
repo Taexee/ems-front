0e9d5036d3c279465518c6a8df32c9c85ab61e84
Taexee <hee66265@naver.com>
2024-01-12 10:08:21 +0900

    feat: 직원 목록 component api 연결

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,27 +1,15 @@
-
+import React, {useEffect, useState} from 'react'
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponent = () => {
-
-  const dummyData = [
-    {
-      "id": 1,
-      "firstName": "Gildong",
-      "lastName": "Hong",
-      "email": "xxx@xxx.xxx"
-    }, 
-    {
-      "id": 2,
-      "firstName": "Sohee",
-      "lastName": "Han",
-      "email": "dsfsdf@xxx.xxx"
-    },
-    {
-      "id": 3,
-      "firstName": "Wonyoung",
-      "lastName": "Jang",
-      "email": "xxx@xxx.xxx"
-    }
-  ]
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    listEmployees().then((response) => {
+      setEmployees(response.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }, []);
 
   return (
     <div className='container'>
@@ -37,7 +25,7 @@ const ListEmployeeComponent = () => {
         </thead>
         <tbody>
           {
-            dummyData.map(employee => 
+            employees.map(employee => 
               <tr key={employee.id}>
                 <td>{employee.id}</td>
                 <td>{employee.firstName}</td>
